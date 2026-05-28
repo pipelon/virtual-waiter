f7c7f63d15fbe47c74c4e3e82a745cb85b85984e
Pipe <pipe.echeverri.1@gmail.com>
2026-05-27 19:46:27 -0500

    resolving conflict

---
+++ whatsapp-engine/src/products/controllers/products.controller.ts
@@ -0,0 +1,26 @@
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+} from '@nestjs/common';
+import { ProductsService } from '../services/products.service';
+
+@Controller({ path: 'products', version: '1' })
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  async findAll() {
+    return await this.productsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    if (isNaN(+id)) {
+      throw new HttpException('Invalid ID', HttpStatus.BAD_REQUEST);
+    }
+    return await this.productsService.findOne(+id);
+  }
+}
---
+++ whatsapp-engine/src/products/entities/product.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  category_id!: number;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  description!: string;
+
+  @Column()
+  price!: number;
+
+  @Column()
+  active!: boolean;
+}
